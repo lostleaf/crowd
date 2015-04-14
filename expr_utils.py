901aee8278c4ea027466258364459f98695f69e9
lostleaf <lostleaf@me.com>
2015-04-14 22:37:24 +0800

    add load dataset function into expr_utils

--- expr_utils.py
+++ expr_utils.py
@@ -2,6 +2,20 @@
 # encoding: utf-8
 
 import numpy as np
+from sklearn.preprocessing import scale
+
+def load_dataset(path, trainset, testset, is_scale=False):
+    feat_cnt = np.load(path)
+    feat, cnt = feat_cnt['feat'], feat_cnt['cnt']
+    if is_scale:
+        feat = scale(feat)
+
+    feat_train = filter_by_ranges(feat, trainset)
+    cnt_train = filter_by_ranges(cnt, trainset)
+    feat_test = filter_by_ranges(feat, testset)
+    cnt_test = filter_by_ranges(cnt, testset)
+
+    return feat_train, cnt_train, feat_test, cnt_test
 
 def filter_by_ranges(arr, rngs):
     data_list = [arr[st:en] for st, en in rngs]
