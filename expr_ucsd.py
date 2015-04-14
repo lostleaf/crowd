89f3e953dca5e2a3531e760d9b9ae11676b91512
lostleaf <lostleaf@me.com>
2015-04-14 22:38:09 +0800

    add rbf svr and scale data first in both linear and rbf svr

--- expr_ucsd.py
+++ expr_ucsd.py
@@ -2,44 +2,48 @@ import numpy as np
 import json
 from sklearn import linear_model
 from sklearn import kernel_ridge
-from sklearn import preprocessing
 from sklearn import svm
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 from matplotlib import pyplot as plt
-from expr_utils import filter_by_ranges, expr
+from expr_utils import filter_by_ranges, expr, load_dataset
 
 def linear(feat_train, cnt_train, feat_test, cnt_test):
     regr = linear_model.LinearRegression()
     expr(regr,  "linear", feat_train, cnt_train, feat_test, cnt_test)
 
 def ridge(feat_train, cnt_train, feat_test, cnt_test):
-    regr = linear_model.Ridge(alpha=0.001)
+    regr = linear_model.Ridge(alpha=1e-3)
     expr(regr, "ridge", feat_train, cnt_train, feat_test, cnt_test)
 
 def lkridge(feat_train, cnt_train, feat_test, cnt_test):
-    regr = kernel_ridge.KernelRidge(alpha=0.0008)
+    regr = kernel_ridge.KernelRidge(alpha=1e-3)
     expr(regr,  "linear kernel ridge regression", feat_train, cnt_train, feat_test, cnt_test)
 
 def lksvr(feat_train, cnt_train, feat_test, cnt_test):
-    regr = svm.LinearSVR(C=1000)
-    expr(regr,  "linear kernel SVR", feat_train, cnt_train, feat_test, cnt_test)
+    scaler = StandardScaler()
+    regr = svm.LinearSVR(C=1e-1)
+    pipeline = Pipeline([('scaler', scaler), ('svr', regr)])
+    expr(pipeline,  "linear kernel SVR", feat_train, cnt_train, feat_test, cnt_test)
+
+def rbfsvr(feat_train, cnt_train, feat_test, cnt_test):
+    scaler = StandardScaler()
+    regr = svm.SVR(C=1e2, gamma=0.0002)
+    pipeline = Pipeline([('scaler', scaler), ('svr', regr)])
+    expr(pipeline,  "rbf kernel SVR", feat_train, cnt_train, feat_test, cnt_test)
 
 def main():
     with open('config.json') as cfg_file:
         cfg = json.load(cfg_file)['vidf']
-    
-    feat_cnt = np.load(cfg['cvt_feat'])
-    feat, cnt = feat_cnt['feat'], feat_cnt['cnt']
 
-    feat_train = filter_by_ranges(feat, cfg['trainset'])
-    cnt_train = filter_by_ranges(cnt, cfg['trainset'])
-    feat_test = filter_by_ranges(feat, cfg['testset'])
-    cnt_test = filter_by_ranges(cnt, cfg['testset'])
+    feat_train, cnt_train, feat_test, cnt_test = load_dataset(cfg['cvt_feat'], cfg['trainset'], cfg['testset'])
 
     # print feat_train.shape, feat_test.shape
     linear(feat_train, cnt_train, feat_test, cnt_test)
     ridge(feat_train, cnt_train, feat_test, cnt_test)
     lkridge(feat_train, cnt_train, feat_test, cnt_test)
     lksvr(feat_train, cnt_train, feat_test, cnt_test)
+    rbfsvr(feat_train, cnt_train, feat_test, cnt_test)
 
 
 if __name__ == '__main__':
