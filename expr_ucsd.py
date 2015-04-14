c76c9822edfd080925300bd943741a7d00cf1b58
lostleaf <jack951753@gmail.com>
2015-04-14 09:12:11 +0800

    add linear kernel SVR to ucsd

--- expr_ucsd.py
+++ expr_ucsd.py
@@ -3,6 +3,7 @@ import json
 from sklearn import linear_model
 from sklearn import kernel_ridge
 from sklearn import preprocessing
+from sklearn import svm
 from matplotlib import pyplot as plt
 
 def filter_by_ranges(arr, rngs):
@@ -18,20 +19,29 @@ def linear(feat_train, cnt_train, feat_test, cnt_test):
     print "MRE: %.2f%%" % (np.mean(np.abs(cnt_pred  - cnt_test) / cnt_test) * 100)
 
 def ridge(feat_train, cnt_train, feat_test, cnt_test):
-    regr = linear_model.Ridge(alpha=0.0008)
+    regr = linear_model.Ridge(alpha=0.001)
     regr.fit(feat_train, cnt_train)
     cnt_pred = regr.predict(feat_test)
     print "ridge"
     print "MAE: %.2f" % np.mean(np.abs(cnt_pred  - cnt_test))
     print "MRE: %.2f%%" % (np.mean(np.abs(cnt_pred  - cnt_test) / cnt_test) * 100)
 
-def kridge(feat_train, cnt_train, feat_test, cnt_test):
+def lkridge(feat_train, cnt_train, feat_test, cnt_test):
     regr = kernel_ridge.KernelRidge(alpha=0.0008)
     regr.fit(feat_train, cnt_train)
     cnt_pred = regr.predict(feat_test)
-    print "linear kernel ridge"
+    print "linear kernel ridge regression"
     print "MAE: %.2f" % np.mean(np.abs(cnt_pred  - cnt_test))
     print "MRE: %.2f%%" % (np.mean(np.abs(cnt_pred  - cnt_test) / cnt_test) * 100)
+
+def lksvr(feat_train, cnt_train, feat_test, cnt_test):
+    regr = svm.LinearSVR(C=0.1)
+    regr.fit(feat_train, cnt_train)
+    cnt_pred = regr.predict(feat_test)
+    print "linear kernel SVR"
+    print "MAE: %.2f" % np.mean(np.abs(cnt_pred  - cnt_test))
+    print "MRE: %.2f%%" % (np.mean(np.abs(cnt_pred  - cnt_test) / cnt_test) * 100)
+
 
 def main():
     with open('config.json') as cfg_file:
@@ -48,8 +58,8 @@ def main():
     # print feat_train.shape, feat_test.shape
     linear(feat_train, cnt_train, feat_test, cnt_test)
     ridge(feat_train, cnt_train, feat_test, cnt_test)
-
-    kridge(feat_train, cnt_train, feat_test, cnt_test)
+    lkridge(feat_train, cnt_train, feat_test, cnt_test)
+    lksvr(feat_train, cnt_train, feat_test, cnt_test)
 
 
 if __name__ == '__main__':
