b502d261fdf730ec51ad2c140e49a22913ad17d5
lostleaf <lostleaf@me.com>
2015-04-17 19:30:28 +0800

    add gpr to all

--- expr_pets.py
+++ expr_pets.py
@@ -7,23 +7,28 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from matplotlib import pyplot as plt
 from expr_utils import filter_by_ranges, expr, load_dataset
+from sklearn.gaussian_process import GaussianProcess
 
 with open('config.json') as cfg_file:
     cfg = json.load(cfg_file)['pets']
 
 feat, cnt = load_dataset(cfg['cvt_feat'])
 
+
 def linear():
     regr = linear_model.LinearRegression()
     expr(regr,  "linear", feat, cnt)
+
 
 def ridge():
     regr = linear_model.Ridge(alpha=18)
     expr(regr, "ridge", feat, cnt)
 
+
 def lkridge():
     regr = kernel_ridge.KernelRidge(alpha=1e1)
     expr(regr,  "linear kernel ridge regression", feat, cnt)
+
 
 def lksvr():
     scaler = StandardScaler()
@@ -31,20 +36,29 @@ def lksvr():
     pipeline = Pipeline([('scaler', scaler), ('svr', regr)])
     expr(pipeline,  "linear kernel SVR", feat, cnt)
 
+
 def rbfsvr():
     scaler = StandardScaler()
     regr = svm.SVR(C=1300, gamma=1e-5)
     pipeline = Pipeline([('scaler', scaler), ('svr', regr)])
     expr(pipeline,  "rbf kernel SVR", feat, cnt)
 
+
+def gpr():
+    scaler = StandardScaler()
+    regr = GaussianProcess(regr='constant', corr='cubic', theta0=0.07)
+    pipeline = Pipeline([('scaler', scaler), ('gpr', regr)])
+    expr(pipeline,  "GPR", feat, cnt)
+
+
+
 def main():
-    # print feat_train.shape, feat_test.shape
+    print feat_train.shape, feat_test.shape
     linear()
     ridge()
-    lkridge()
     lksvr()
     rbfsvr()
-
+    gpr()
 
 if __name__ == '__main__':
     main()
--- expr_ucsd.py
+++ expr_ucsd.py
@@ -1,9 +1,10 @@
 import numpy as np
 import json
+import math
 from sklearn import linear_model
 from sklearn import kernel_ridge
 from sklearn import svm
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, scale
 from sklearn.pipeline import Pipeline
 from sklearn.gaussian_process import GaussianProcess
 from matplotlib import pyplot as plt
@@ -38,19 +39,23 @@ def rbfsvr():
     pipeline = Pipeline([('scaler', scaler), ('svr', regr)])
     expr(pipeline,  "rbf kernel SVR", feat, cnt)
 
+def ker(x, y):
+    s = x - y
+    return np.dot(x, y) #+ math.exp(-np.dot(s, s))
+
 def gpr():
     scaler = StandardScaler()
-    regr = GaussianProcess(regr='linear', corr='linear', optimizer='Welch')
+    regr = GaussianProcess(regr='linear', corr='linear', theta0=0.3)
     pipeline = Pipeline([('scaler', scaler), ('gpr', regr)])
-    expr(pipeline,  "GPR", feat, cnt)
+    expr(pipeline,  "GPR", scale(feat), cnt)
 
 def main():
     # print feat_train.shape, feat_test.shape
-    # linear()
-    # ridge()
+    linear()
+    ridge()
     # lkridge()
-    # lksvr()
-    # rbfsvr()
+    lksvr()
+    rbfsvr()
     gpr()
 
 
